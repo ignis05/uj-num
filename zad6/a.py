68bc671dddaa5ebe922e174b48c061d8032b24bb
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-12-27 16:09:51 +0100

    num6: cleanup

--- zad6/a.py
+++ zad6/a.py
@@ -15,8 +15,9 @@ def nodeGen1(i, n):
     return -1 + 2*(i/n)
 
 
-# f1
 x = np.linspace(-1, 1, 1000)
+
+# f1
 nList = [2, 3, 4, 6, 8, 10]
 # orig:
 plt.subplot(1, 2, 1)
@@ -26,12 +27,10 @@ plt.plot(x, f1(x), label='$f(x)$', linewidth=5)
 # interpolations
 for k, n in enumerate(nList):
     inter = generateInterpolation([[nodeGen1(i, n), f1(nodeGen1(i, n))] for i in range(n+1)])
-    x = np.linspace(-1, 1, 1000)
     plt.plot(x, inter(x), label='$W_{'+str(n)+'}(x)$')
 plt.legend(loc='lower center')
 
 # f2
-x = np.linspace(-1, 1, 1000)
 nList = [2, 3, 4, 6, 8, 48]
 # orig:
 plt.subplot(1, 2, 2)
@@ -41,7 +40,6 @@ plt.plot(x, f2(x), label='$f(x)$', linewidth=5)
 # interpolations
 for k, n in enumerate(nList):
     inter = generateInterpolation([[nodeGen1(i, n), f2(nodeGen1(i, n))] for i in range(n+1)])
-    x = np.linspace(-1, 1, 1000)
     plt.plot(x, inter(x), label='$W_{'+str(n)+'}(x)$')
 plt.legend(loc='lower center')
 
--- zad6/b.py
+++ zad6/b.py
@@ -15,8 +15,9 @@ def nodeGen2(i, n):
     return np.cos(((2*i+1)/(2*(n+1)))*np.pi)
 
 
-# f1
 x = np.linspace(-1, 1, 1000)
+
+# f1
 nList = [4, 8, 16, 24, 48, 96]
 # orig:
 plt.subplot(1, 2, 1)
@@ -26,12 +27,10 @@ plt.plot(x, f1(x), label='$f(x)$', linewidth=5)
 # interpolations
 for k, n in enumerate(nList):
     inter = generateInterpolation([[nodeGen2(i, n), f1(nodeGen2(i, n))] for i in range(n+1)])
-    x = np.linspace(-1, 1, 1000)
     plt.plot(x, inter(x), label='$W_{'+str(n)+'}(x)$')
 plt.legend(loc='lower center')
 
 # f2
-x = np.linspace(-1, 1, 1000)
 nList = [2, 3, 4, 6, 8, 48]
 # orig:
 plt.subplot(1, 2, 2)
@@ -41,7 +40,6 @@ plt.plot(x, f2(x), label='$f(x)$', linewidth=5)
 # interpolations
 for k, n in enumerate(nList):
     inter = generateInterpolation([[nodeGen2(i, n), f2(nodeGen2(i, n))] for i in range(n+1)])
-    x = np.linspace(-1, 1, 1000)
     plt.plot(x, inter(x), label='$W_{'+str(n)+'}(x)$')
 plt.legend(loc='lower center')
 
