d068cde11c33bed5aff4feff1e00b8be009ee712
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-12-28 21:56:12 +0100

    num7: part1 done

--- zad7/program.py
+++ zad7/program.py
@@ -22,9 +22,6 @@ def createSpline(n):
     matrix = sparse.diags([1, 4, 1], [-1, 0, 1], shape=(n-1, n-1)).tocsr()
     ksi = spsolve(matrix, wektorY)
 
-    print(f'xi: {xi}')
-    print(f'yi: {yi}')
-
     def KSIi(i):
         if i == 0 or i == n:
             return 0
@@ -34,24 +31,31 @@ def createSpline(n):
     def Bi(i): return yi[i-1] - KSIi(i-1)*((h**2)/6)
 
     def spline(x):
-        i = [i for i, val in enumerate(xi) if val >= x][0]
-        y = (KSIi(i-1) * ((xi[i]-x)**3)) + (KSIi(i) * ((x-xi[i-1])**3)) + (Ai(i)*(x-xi[i-1])) + Bi(i)
-        print(f'x={x}, y={y}')
+        # sprawdzenie w ktorym przedziale jest x
+        i = len(xi)-1
+        for index, val in enumerate(xi):
+            if val > x:
+                i = index
+                break
+        y = (KSIi(i-1) * ((xi[i]-x)**3)/(6*h)) + (KSIi(i) * ((x-xi[i-1])**3)/(6*h)) + (Ai(i)*(x-xi[i-1])) + Bi(i)
         return y
 
-    return spline
+    return spline, xi, yi
 
 
 linspace = np.linspace(-1, 1, 100)
-nList = [4]
+nList = [3, 4, 5, 6, 8, 16]
 # orig:
-# plt.title(r'Wielomiany interpolacyjne funkcji $f_1(x)=\frac{1}{1+25x^{2}}$ w zależności od stopnia n')
+plt.title(
+    'Interpolacja funkcji $f(x)=\\frac{1}{1+25x^{2}}$ za pomocą naturalnych splajnów kubicznych przechodzących przez punkty $x_i=-1+2\\frac{i}{n}$ dla kilku wybranych $n$.')
 plt.grid()
 plt.plot(linspace, f(linspace), label='$f(x)$', linewidth=5)
+plt.scatter([0], [1], marker='.', zorder=0)  # so the marker colors are in sync
 # interpolations
-for k, n in enumerate(nList):
-    spline = createSpline(n)
+for index, n in enumerate(nList):
+    spline, dotsX, dotsY = createSpline(n)
     plt.plot(linspace, [spline(x) for x in linspace], label='$S_{'+str(n)+'}(x)$')
+    plt.scatter(dotsX, dotsY, zorder=(len(nList)*10)-(index*10), marker='+')
 plt.legend(loc='lower center')
 
 plt.show()
