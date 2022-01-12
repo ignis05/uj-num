5bed2b757ea7dd9a0cff9c641eb0228969ac9780
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-01-13 00:23:29 +0100

    num9: done

--- zad9/program.py
+++ zad9/program.py
@@ -1,6 +1,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from common import *
+from matplotlib.ticker import MaxNLocator
+from funkcje import *
 
 
 def f(x):
@@ -11,25 +12,60 @@ def g(x):
     return (np.sin(x)-0.37)**2
 
 
-xStar = np.arcsin(0.37)
-print(xStar)
+def gPrim(x):
+    return 2 * (np.sin(x)-0.37) * np.cos(x)
 
-MAXI = 10
+
+def u(x):
+    return g(x)/gPrim(x)
+
+
+def uPrim(x):
+    return ((np.sin(x)-0.37)*np.tan(x)*np.arcsin(x)+1) / 2
+
+
+xStar = np.arcsin(0.37)
+
+x2Star = np.pi + np.arcsin(0.37)
 
 plt.subplot(1, 2, 1)
 plt.title('Dla funkcji $ f(x)=sin(x)-0.37 $')
 plt.yscale('log')
 plt.grid()
-bisectRes = bisect(f, 0, np.pi/2, MAXI)
+bisectRes = bisect(f, 0, np.pi/2, 20)
 plt.plot([i+1 for i in range(len(bisectRes))], [abs(xStar - xi) for xi in bisectRes], '-o', label='Metoda bisekcji', )
-falsiRes = falsi(f, 0, np.pi/2, MAXI)
+falsiRes = falsi(f, 0, np.pi/2, 13)
 plt.plot([i+1 for i in range(len(falsiRes))], [abs(xStar - xi) for xi in falsiRes], '-o', label='Metoda falsi', )
-secantsRes = secants(f, 1, np.pi/2, MAXI)
+secantsRes = secants(f, 0, np.pi/2, 10)
 plt.plot([i+1 for i in range(len(secantsRes))], [abs(xStar - xi) for xi in secantsRes], '-o', label='Metoda siecznych', )
-newtonRes = newton(f, np.cos, 1, MAXI)
+newtonRes = newton(f, np.cos, 1, 10)
 print([abs(xStar - xi) for xi in newtonRes])
 plt.plot([i+1 for i in range(len(newtonRes))], [abs(xStar - xi) for xi in newtonRes], '-o', label='Metoda Newtona', )
 plt.gca().set_yticks([1, 0.01, 0.0001, 0.000001, 0.00000001, 0.0000000001, 0.000000000001, 0.00000000000001, 0.0000000000000001, ])
-plt.legend()
+plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.xlabel('iteracja')
+plt.ylabel('$|x* - x_i|$')
+plt.legend(loc='upper right')
 
+plt.subplot(1, 2, 2)
+plt.title('Dla funkcji $ g(x)=(sin(x)-0.37)^2 $  ( dla $u(x)=\\frac{g(x)}{g\'(x)}$)')
+plt.yscale('log')
+plt.grid()
+# * wymagaja przeciwnych znakow, g(x) ma tylko wartosci nieujemne
+# bisectRes = bisect(g, 0, np.pi/2, MAXI)
+# plt.plot([i+1 for i in range(len(bisectRes))], [abs(xStar - xi) for xi in bisectRes], '-o', label='Metoda bisekcji', )
+# falsiRes = falsi(g, 0, np.pi/2, MAXI)
+# plt.plot([i+1 for i in range(len(falsiRes))], [abs(xStar - xi) for xi in falsiRes], '-o', label='Metoda falsi', )
+secantsRes = secants(u, 0, np.pi/2, 10)
+plt.plot([i+1 for i in range(len(secantsRes))], [abs(xStar - xi) for xi in secantsRes], '-o', label='Metoda siecznych', color='C2')
+newtonRes = newton(u, uPrim, 1, 10)
+print([abs(xStar - xi) for xi in newtonRes])
+plt.plot([i+1 for i in range(len(newtonRes))], [abs(xStar - xi) for xi in newtonRes], '-o', label='Metoda Newtona', color='C3')
+plt.gca().set_yticks([1, 0.01, 0.0001, 0.000001, 0.00000001, 0.0000000001, 0.000000000001, 0.00000000000001, 0.0000000000000001, ])
+plt.xlabel('iteracja')
+plt.ylabel('$|x* - x_i|$')
+plt.legend(loc='upper right')
+
+plt.tight_layout()
+plt.suptitle('Odchylenie przybliżonej wartości pierwiastka od dokładnego wyniku w zależności od wybranych funkcji i metod iteracyjnych')
 plt.show()
